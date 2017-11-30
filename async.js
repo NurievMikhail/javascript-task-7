aeb89070fd3063a3e1effec239db72fa82912715
NurievMikhail <nurievmm@yandex.ru>
2017-11-30 16:30:12 +0500

    Решение задачи.

--- async.js
+++ async.js
@@ -7,7 +7,38 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise((resolve) => {
+        if (jobs.length === 0 || parallelNum <= 0) {
+            return resolve([]);
+        }
+
+        let index = 0;
+        let resultJobs = [];
+        const timeoutJobs = jobs.map(job =>
+            () => new Promise((jobResolve, jobReject) => {
+                job().then(jobResolve, jobReject);
+                setTimeout(() => (jobReject(new Error('Promise timeout'))), timeout);
+            })
+        );
+        const emitFunction = (result) => {
+            resultJobs[index] = result;
+            if (timeoutJobs.length > index) {
+                timeoutJobs[index]().then(emitFunction, emitFunction);
+                index++;
+            }
+            if (timeoutJobs.length === resultJobs.filter((a) => (a !== undefined)).length) {
+                resolve(resultJobs);
+            }
+        };
+
+        timeoutJobs
+            .slice(0, parallelNum)
+            .forEach((job) => {
+                job().then(emitFunction, emitFunction);
+                index++;
+            });
+    });
 }
